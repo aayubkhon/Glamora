4d82587260bf943dfae36867c253036ef1cc466a
aayubxon701 <aayubxon701@gmail.com>
2025-08-05 20:28:03 +0900

    feat: dotenv & project configrations

---
+++ server.js
@@ -0,0 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const http = require("http");
+const mongodb = require("mongodb");
+
+const connectionString = process.env.MONGO_URL;
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDb");
+    else {
+      console.log("MongoDb connection secceed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = process.env.PORT || 3000;
+      server.listen(PORT, function () {
+        console.log(`The server on port: ${PORT}, http://localhost:${PORT}`);
+      });
+    }
+  }
+);
