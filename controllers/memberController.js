b5e184647f5400824f6cf30647b8ac7709d8464a
aayubxon701 <aayubxon701@gmail.com>
2025-08-07 22:08:10 +0900

    feat: bcrypt package assosida passwordni hash qilish

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -5,24 +5,24 @@ memberController.signup = async (req, res) => {
   try {
     console.log("POST: cont/signup");
     const data = req.body;
-    const member = new Member();
-    const new_member = await member.signupData(data);
-    res.json({state: "success",data:new_member})
+    member = new Member();
+    new_member = await member.signupData(data);
+    res.json({ state: "success", data: new_member });
   } catch (err) {
     console.log(`ERROR, cont/signup,${err.message}`);
-    res.json({state: "fail",message: err.message})
+    res.json({ state: "fail", message: err.message });
   }
 };
 memberController.login = async (req, res) => {
-    try {
+  try {
     console.log("POST: cont/login");
     const data = req.body;
     member = new Member();
     result = await member.loginData(data);
-    res.json({state: "success",data:result})
+    res.json({ state: "success", data: result });
   } catch (err) {
     console.log(`ERROR, cont/login,${err.message}`);
-    res.json({state: "fail",message: err.message})
+    res.json({ state: "fail", message: err.message });
   }
 };
 
